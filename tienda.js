58ccdb7736dcf02f045ce3c85a607a4bb886e6a7
cristian salazar <salazarcristiand@gmail.com>
2022-08-21 14:40:10 -0500

    nuevos cambios, cree un objeto con los cupones

--- tienda.js
+++ tienda.js
@@ -5,20 +5,41 @@ const answerCalculate = document.querySelector('#answer');
 const btnCalculate = document.querySelector('#btnC');
 btnCalculate.addEventListener('click', calculatePercent);
 
-class coupon{
+/*class coupon{
     constructor(name, discount){
         this.name = name,
         this.discount = discount
     }
-};
+}
 const coupons = [];
 
-coupons.push(new coupon('gold', 50), new coupon('silver', 30))
+//coupons.push(new coupon('gold', 50), new coupon('silver', 30))
+*/
+
+const coupons= {
+    gold: 30,
+    silver: 25,
+    bronze: 10
+}
 
 function calculatePercent(){
  //(p * (100 -d )) / 100
 
- const validCoupon = coupons.find(item => item.name == couponInput.value);
+ const price=Number(inputPrice.value);
+ const coupon = couponInput.value;
+
+ console.log({price,discount});
+  let discount;
+
+  if(coupons[coupon]){
+    discount = coupons[coupn];
+  }else{
+    answerCalculate.innerText = "Cupón no válido"
+  }
+
+
+
+ //const validCoupon = coupons.find(item => item.name == couponInput.value);
 
 
 }
